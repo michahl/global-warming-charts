f56646687c4f0c78aa4e2ed2b90bbf74cb56b641
Michail Christoforatos <mchristoforatos@icloud.com>
2024-10-03 23:22:52 +0300

    updated header

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,25 +1,25 @@
 import Image from "next/image"
+import Link from "next/link"
 import logo from '../../assets/logo.png'
 
 export default function Navbar() {
     return (
         <nav className="navbar bg-gray-700/35 rounded-2xl p-2 sm:mx-2 md:mx-0">
-            <a className="navbar-start" href="/">
-                <Image
-                    src={logo}
-                    width={80}
-                    height={80}
-                    alt="logo"
-                    className="btn btn-ghost btn-square p-1"
-                />
-            </a>
-            <div className="navbar-center">
-                <a className="font-semibold text-lg" href="/">Green Habits</a>
+            <div className="navbar-start">
+                <Link href="/">
+                    <Image
+                        src={logo}
+                        width={80}
+                        height={80}
+                        alt="logo"
+                        className="btn btn-ghost btn-square p-1"
+                    />
+                </Link>
             </div>
             <div className="navbar-end">
-                <a href="/login" className="px-5 btn btn-success rounded-3xl uppercase text-white">
+                <Link href="/login" className="px-5 btn btn-success rounded-3xl uppercase text-white">
                     login
-                </a>
+                </Link>
             </div>
         </nav>
     )
