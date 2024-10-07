9753af7854159b80a6defa22fe528d781d08421c
Michail Christoforatos <mchristoforatos@icloud.com>
2024-10-07 21:35:15 +0300

    update chart style

--- src/app/components/AvgTemperatureChart.js
+++ src/app/components/AvgTemperatureChart.js
@@ -1,36 +1,84 @@
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, ReferenceLine, Dot } from 'recharts';
+import React, { useEffect, useState } from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, ReferenceLine, ReferenceDot } from 'recharts';
 
 const TemperatureChart = ({ data = [] }) => {
     const lastYear = 2024;
     const lastDataPoint = data.find(d => d.year === lastYear);
 
+    const [xTicks, setXTicks] = useState([1880, 1900, 1920, 1940, 1960, 1980, 2000, 2024]);
+    const [yTicks, setYTicks] = useState([11, 12, 13, 14, 15]);
+    const [hoveredData, setHoveredData] = useState(null);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 800) {
+                setXTicks([1890, 1900, 1920, 1940, 1960, 1980, 2000, 2024]);
+                setYTicks([11, 13, 15]);
+            } else {
+                setXTicks([1890, 1900, 1920, 1940, 1960, 1980, 2000, 2024]);
+                setYTicks([11, 12, 13, 14, 15]);
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize(); // Call initially to set the correct ticks
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const handleMouseMove = (e) => {
+        if (e && e.activePayload) {
+            setHoveredData(e.activePayload[0].payload);
+        } else {
+            setHoveredData(null);
+        }
+    };
+
+    const displayData = hoveredData || lastDataPoint;
+
     return (
-        <ResponsiveContainer width="100%" height={300}>
-            <AreaChart data={data}>
-                <CartesianGrid vertical={false} horizontal={false} />
-                <XAxis 
-                    dataKey="year" 
-                    ticks={[1880, 1890, 1900, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010, 2024]} 
-                    tick={{ fill: 'white', fontSize: 12 }} // Adjust font size here
-                    axisLine={false}
-                    tickLine={false}
-                />
-                <YAxis 
-                    domain={[11, 'dataMax']} 
-                    ticks={[11, 12, 13, 14, 15]} 
-                    tick={{ fill: 'white', fontSize: 12 }} // Adjust font size here
-                    axisLine={false}
-                    tickLine={false}
-                />
-                <Tooltip />
-                <Legend />
-                <ReferenceLine y={13.84} stroke="#A9A9A9" strokeDasharray="3 3" />
-                <Area type="monotone" dataKey="averageTemp" fill="#d3d3d3" stroke="#d3d3d3" />
-                {lastDataPoint && (
-                    <Dot cx={lastDataPoint.year} cy={lastDataPoint.averageTemp} r={5} fill="red" />
-                )}
-            </AreaChart>
-        </ResponsiveContainer>
+        <div style={{ position: 'relative' }}>
+            <div style={{ position: 'absolute', top: 5, right: 35, color: 'white', zIndex: 10 }}>
+                {displayData ? (
+                    <div className='flex flex-col items-end leading-none'>
+                        <p className='font-medium text-[1.05rem]'>{displayData.year}</p>
+                        <p className='text-gray-500/90 text-xs'>in °C </p>
+                        <p className='font-semibold text-[1.05rem]'>{displayData.averageTemp}</p>
+                    </div>  
+                ) : ''}
+            </div>
+            <ResponsiveContainer width="100%" height={300}>
+                <AreaChart data={data} margin={{ top: 75, right: 35, left: 0, bottom: 10 }} onMouseMove={handleMouseMove} onMouseLeave={() => setHoveredData(null)}>
+                    <CartesianGrid vertical={false} horizontal={false} />
+                    <XAxis 
+                        dataKey="year" 
+                        ticks={xTicks} 
+                        tick={{ fill: 'white', fontSize: 12 }} // Adjust font size here
+                        axisLine={false}
+                        tickLine={false}
+                    />
+                    <YAxis 
+                        domain={[11, 'dataMax']} 
+                        ticks={yTicks} 
+                        tick={{ fill: 'white', fontSize: 12 }} // Adjust font size here
+                        axisLine={false}
+                        tickLine={false}
+                    />
+                    <Tooltip 
+                        contentStyle={{ backgroundColor: '#333', border: 'none' }} 
+                        labelStyle={{ color: 'white' }} 
+                        itemStyle={{ color: 'white' }} 
+                    />
+                    <ReferenceLine y={13.84} stroke="#8A8A8A" strokeDasharray="3 3" />
+                    <Area type="monotone" dataKey="averageTemp" fill="#8A8A8A" stroke="#8A8A8A" />
+                    {lastDataPoint && (
+                        <>
+                            <ReferenceDot x={lastDataPoint.year} y={lastDataPoint.averageTemp} r={4} fill="#8A8A8A" />
+                        </>
+                    )}
+                </AreaChart>
+            </ResponsiveContainer>
+        </div>
     );
 };
 
